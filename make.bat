6c29dc68bacd9fab05b90c80436b84fa62436f80
LocoMJ <sergiopresafontan@gmail.com>
2016-07-04 13:27:59 +0200

    Updateado el sonido y FX

--- make.bat
+++ make.bat
@@ -2,12 +2,16 @@ REM graphics
 lcc -Wf-bo1 -c dev/data/graphics/sprite_tileset.c -o sprite_tileset.o
 lcc -Wf-bo1 -c dev/data/graphics/sprite_bkg_tileset.c -o sprite_bkg_tileset.o
 lcc -Wf-bo1 -c dev/data/graphics/bkg_tileset.c -o bkg_tileset.o 
+REM music
+lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o music.o dev/data/music/music.c
+lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o gbt_player.o dev/data/music/gbt_player.s
+lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o gbt_player_bank1.o dev/data/music/gbt_player_bank1.s
 REM maps
 lcc -Wf-bo2 -c dev/data/maps/level0_map.c -o level0_map.o
 lcc -Wf-bo2 -c dev/data/maps/gui_map.c -o gui_map.o
 REM logic
 lcc -c dev/main.c -o main.o
 REM build
-lcc -Wl-yt1 -Wl-yo4 -Wl-ya0 main.o sprite_tileset.o sprite_bkg_tileset.o bkg_tileset.o level0_map.o gui_map.o dev/utils/math.s dev/utils/fixes.s -o rom/red_hot_princess_carnage.gb
+lcc -Wl-yt1 -Wl-yo4 -Wl-ya0 main.o sprite_tileset.o sprite_bkg_tileset.o bkg_tileset.o level0_map.o gui_map.o music.o gbt_player.o gbt_player_bank1.o dev/utils/math.s dev/utils/fixes.s -o rom/red_hot_princess_carnage.gb
 REM clean
-del *.o
+del *.o *.lst
